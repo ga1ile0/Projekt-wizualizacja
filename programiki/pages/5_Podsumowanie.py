8eb09114b7d0aa9a34ffbe80260c06488ae47d6a
sosna0 <132777824+sosna0@users.noreply.github.com>
2023-06-23 09:15:45 +0200

    dodano tekst

--- programiki/pages/5_Podsumowanie.py
+++ programiki/pages/5_Podsumowanie.py
@@ -2,11 +2,12 @@ import streamlit as st
 from PIL import Image
 from graph_functions import piechart_average
 
+
 st.title("Podsumowanie")
 st.divider()
 
 col1, col2, col3 = st.columns([1, 15, 1])
-image = Image.open("../tytułowa.jpeg")#do zmiany zdjęcie
+image = Image.open("../podsum.jpg")#do zmiany zdjęcie
 
 with col1:
     st.write(' ')
@@ -15,6 +16,16 @@ with col2:
 with col3:
     st.write(' ')
 
+st.divider()
 piechart_average()
+st.divider()
+
+st.write('''
+    Widzimy, że jeżeli jest duży popyt, to ceny rosną - najwięcej zapłacimy za
+    pobyt nad Morzem, ale też najwięcej Polaków spędza tam swoje wakacje.
+    
+    W przypadku gór oraz Mazur, ceny są bardzo zbliżone, co również pokrywa się
+    ze statystykami.
+''')
 
 st.divider()
